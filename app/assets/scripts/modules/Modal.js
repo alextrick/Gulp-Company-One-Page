e9cb44f5a96af5f5fd4c83b03b91ec4a80b70c9b
Alex <alexwilsteadbriggs@gmail.com>
2017-11-27 15:06:20 +0000

    Modal completed

---
+++ app/assets/scripts/modules/Modal.js
@@ -0,0 +1,37 @@
+import $ from "jquery";
+
+class Modal {
+    constructor() {
+        this.openModalButton = $(".open-modal");
+        this.modal = $(".modal");
+        this.closeModalButton = $(".modal__close");
+        this.events();
+    }
+
+    events() {
+        //opens modal
+        this.openModalButton.click(this.openModal.bind(this));
+        //closes modal
+        this.closeModalButton.click(this.closeModal.bind(this));
+        
+        $(document).keyup(this.keyPressHandler.bind(this));
+    }
+
+    openModal() {
+        this.modal.addClass("modal--is-visible");
+        return false;
+    }
+
+    closeModal() {
+        this.modal.removeClass("modal--is-visible");
+    }
+
+    keyPressHandler(e) {
+        if (e.keyCode = 27) {
+            this.closeModal();
+        }
+    }
+
+}
+
+export default Modal;
